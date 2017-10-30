9a2cee82891d0a2700504d29bf601c9ed1d34622
Omkar Ramachandran <omkar.ramachandran@colorado.edu>
2017-10-30 00:15:40 -0600

    Build functions can go up to pi/2 in angle

--- build_matrix.py
+++ build_matrix.py
@@ -11,28 +11,38 @@ import numpy as np
 import math
 
 # Grid dimensions
-N = 60
+N = 3
 # Number of rays per angle
-m = 100
+m = 10
 # Number of points per ray
 n = 20
 
-theta_i = 0
-theta_f = np.pi/4.
+# Generate Theta array
+theta_i = np.pi/2.
+theta_f = np.pi/2.
 Ntheta = 1
 th = np.linspace(theta_i,theta_f,Ntheta)
 
+# Define shape of A matrix
 A = np.zeros([m*Ntheta,N**2])
 
 for t in range(Ntheta):
+	# Define straight line corresponding to the ray
 	x = np.linspace(-np.sqrt(2),np.sqrt(2),n)
 	C = np.linspace(-np.sqrt(2),np.sqrt(2),m)
 	y = np.zeros([m,n])
 	for i in range(m):
-		y[i] = x*np.tan(th[t])+C[i]
+		if(th[t] > np.pi/4.):
+			y[i] = x/np.tan(th[t])+C[i]
+		else:
+			y[i] = x*np.tan(th[t])+C[i]
+
 		for j in range(n):
 			if((x[j] >= -1 and x[j] <= 1) and (y[i][j] >= -1 and y[i][j] <= 1)):
-				X = int((x[j]+1)/2./np.sqrt(2)*(N))
-				Y = int((y[i][j]+1)/2./np.sqrt(2)*(N))
-				print(X,Y,":",x[j],y[i,j])
-				A[t*Ntheta+i,N*Y+X] += 1
+				X = int((x[j]+1)/2.*(N))
+				Y = int((y[i][j]+1)/2.*(N))
+				print(X,Y,":",x[j],y[i,j],":",j,":",m)
+				if(th[t] > np.pi/4.):
+					A[t*Ntheta+i,N*X+Y] += 1
+				else:
+					A[t*Ntheta+i,N*X+Y] += 1
