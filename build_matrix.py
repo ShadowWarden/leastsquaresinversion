550bbc34c28b4bcd63ff1da967ebaf74abcc1274
Omkar Ramachandran <omkar.ramachandran@gmail.com>
2017-11-02 21:41:49 -0600

    The code now successfully reconstructs the CU logo image with an element by element RMS error of O(e-15)

--- build_matrix.py
+++ build_matrix.py
@@ -21,32 +21,52 @@ __author__ = "Omkar H. Ramachandran"
 import numpy as np
 import math
 
+
+def update(A,X,Y,N,th,b,S):
+    for i in range(len(X)):
+        if(X[i] >= N or Y[i] >= N or Y[i] < 0 or X[i] < 0):
+            continue
+        if(abs(th) < np.pi/4):
+            A[N*Y[i]+X[i]] += 1
+            b += S[2*Y[i],2*X[i]]
+        else:
+            A[N*X[i]+Y[i]] += 1
+            b += S[2*X[i],2*Y[i]]
+    return A, b
 # Grid dimensions
-N = 3
+N = 31
 # Number of rays per angle
-m = 10
+m = 2*N
 # Number of points per ray
-n = N**3
+n = 2*N
 
 # Generate Theta array
-theta_i = 0.
+theta_i = -np.pi/2.
 theta_f = np.pi/2.
-Ntheta = 100
+Ntheta = 180
 th = np.linspace(theta_i,theta_f,Ntheta)
 
 # Define shape of A matrix
 A = np.zeros([m*Ntheta,N**2])
+b = np.zeros([m*Ntheta])
+
+#xx = np.linspace(-1,1,N)
+#yy = np.linspace(-1,1,N)
+
+#Xx,Yy = np.meshgrid(xx,yy)
+
+#S = RHO0*np.heaviside(1.-Xx**2-Yy**2,0.5)*(0.7-Xx**2-Yy**2)
+
+S = np.genfromtxt("CU_logo.csv",delimiter=",")
 
 # Do the cot theta division and function call outside the loop.
 # multiplication is ~ 40 times faster chip level than sin/cos/tan
-tanth = np.tan(th)
-cotth = 1./np.tan(th)
-
+        
 for t in range(Ntheta):
-	x = np.linspace(-1,1,n)
-	C = np.linspace(-1,1,m)
-	y = np.zeros([m,n])
-	for i in range(m):
+        x = np.linspace(-np.sqrt(2),np.sqrt(2),n)
+        C = np.linspace(-np.sqrt(2),np.sqrt(2),m)
+        y = np.zeros([m,n])
+        for i in range(m):
             # Define straight line corresponding to the ray
             # Because y is a 2D array and x is 1D, to accomodate the floating
             # constant, I'm not changing names when switching from equation
@@ -55,19 +75,29 @@ for t in range(Ntheta):
             # sampled while for pi/4<th<pi/2, every point in y is sampled,
             # thus it is sufficient to define one spanning vector and define
             # the other in terms of it
-            if(th[t] > np.pi/4.):
-                y[i] = x*cotth[t]+C[i]
+            if(abs(th[t]) > np.pi/4.):
+                y[i] = -abs(th[t])/th[t]*(x*np.tan(np.pi/2 - abs(th[t])) -C[i])
             else:
-                y[i] = x*tanth[t]+C[i]
+                y[i] = -abs(th[t])/th[t]*(x*np.tan(th[t]) - C[i])
                 
             X = (x+1)/2.*(N)
             Y = (y[i]+1)/2.*(N)
             # Ignore points exactly on the boundary of the grid
-            ii = np.where(abs(X) >= N)
-            jj = np.where(abs(Y) >= N)
-            X[ii] = N-1
-            Y[jj] = N-1
+            # ii = np.unique(np.append(ii,jj))
+            # X = np.delete(X,ii)
+            # Y = np.delete(Y,ii)
+            # X[ii] = N-1
+            # Y[jj] = N-1
             X = X.astype(int)
             Y = Y.astype(int)
-            # ... Otherwise, tally up that point in A
-            A[t*m+i,N*X[:]+Y[:]] += 1
+            # ... Otherwise, tally up that point in A. Vectorize for
+            # maximum performance
+            A[t*m+i], b[t*m+i] = update(A[t*m+i],X,Y,N,th[t],b[t*m+i],S)
+
+
+ii = np.where(np.all(A == 0, axis=1))
+A = A[~np.all(A == 0, axis=1)]
+b = np.delete(b,ii)
+#for i in range(len(A)):
+#    A[i] /= np.linalg.linalg.norm(A[i])
+#    b[i] /= np.linalg.linalg.norm(A[i])
